d4e7bb8559672f888c1971b4bb7007a4ca6f1af7
phuongzzz <yesterdayoncemore23@gmail.com>
2018-03-25 23:54:07 +0700

    done registration + login

--- app/src/main/java/com/phuongzzz/gogreen/MainActivity.java
+++ app/src/main/java/com/phuongzzz/gogreen/MainActivity.java
@@ -1,13 +1,41 @@
 package com.phuongzzz.gogreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStaff, mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStaff = (Button) findViewById(R.id.staff);
+        mManager = (Button) findViewById(R.id.manager);
+
+        mStaff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, StaffLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, ManagersLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
